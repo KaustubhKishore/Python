d5bad4284db092eac89eed54bfd43eb920eeb4fa
sachinkumar26 <s.jangir26@gmail.com>
2017-10-24 23:23:05 +0530

    Update class3.py
    Removed Comments

--- class3.py
+++ class3.py
@@ -29,20 +29,20 @@ print 'Sum of digits is: ',sum
 
 
 
-#
-# int i,j,n,k;
-# n=4;
-# for (i=0;i<n;i++)
-#        {
-# 	       	for(j=0;j<n-i;j++)
-# 	       	{
-# 	       		printf(" ");
-# 	       	}
-# 	       	for(k=0;k<i+1;k++)
-# 	       	{
-# 	       		printf("* ");
-# 	       	}
-# 	       	printf("\n");
+
+ int i,j,n,k;
+ n=4;
+ for (i=0;i<n;i++)
+        {
+ 	       	for(j=0;j<n-i;j++)
+ 	       	{
+ 	       		printf(" ");
+ 	       	}
+ 	       	for(k=0;k<i+1;k++)
+ 	       	{
+ 	       		printf("* ");
+ 	       	}
+ 	       	printf("\n");
 
 l1=[10,20,30,10,40,'Abc']
 l2=['abc','xyz']
