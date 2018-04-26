0aa6324b3d59c4d07303e5e954682be590b07a16
Kaustubh Kishore <Kaustubhkishorem@gmail.com>
2018-04-27 01:48:53 +0530

    Delete Empty_dict.py

--- Empty_dict.py
+++
@@ -1,5 +0,0 @@
-# Write a Python program to check if all dictionaries in a list are empty or not.
-# Sample list : [{},{},{}]
-# Return value : True
-# Sample list : [{1,2},{},{}]
-# Return value : False
